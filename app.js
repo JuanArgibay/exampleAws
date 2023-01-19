fe2fed18d3747e5d137030d3b0533835cc0a5e6b
ArgyDev <jmargibayg@gmail.com>
2023-01-19 20:56:36 +0100

    port env added

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+const port = process.env.PORT || 5001;
+
 app.get('/', (req, res) => res.json('Root call successful!'));
 
-app.listen(5001, () => { console.log('this seeems te be up & running')})
+app.listen(port, () => { console.log('this seeems te be up & running')})
